ea53881f77282bda12f598e1963efdd353a156b5
badiou <ourobadiou@gmail.com>
2021-07-05 11:54:11 +0000

    first commit

---
+++ android/app/src/main/kotlin/sampa/com/iamrich/MainActivity.kt
@@ -0,0 +1,6 @@
+package sampa.com.iamrich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+//The main function is starting
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Center
+              (child: Text('I am rich')),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: AssetImage(
+                  'images/diamond.png'
+              ),
+            ),
+          )),
+    ),
+  );
+}
